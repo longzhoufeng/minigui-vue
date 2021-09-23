e38ccb779e3bacaea585716aa8733d6da14f0d23
undefined <undefined>
2021-09-23 15:10:34 +0800

    init

---
+++ antdv-demo/components/api.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="markdown api-container">
+    <slot v-if="isZhCN" name="cn" />
+    <slot v-else />
+  </div>
+</template>
+<script>
+import { isZhCN } from '../utils/util';
+
+const showAd = location.host.indexOf('antdv.com') > -1;
+export default {
+  name: 'Api',
+  components: {},
+  inject: {
+    demoContext: { default: {} },
+  },
+  data() {
+    return {
+      showAd,
+      isZhCN: isZhCN(this.demoContext.name),
+    };
+  },
+};
+</script>
